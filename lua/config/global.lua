7a97ca02887981544f3f8a0d58d0b559e3cef8b3
Alex <alexbstl@gmail.com>
2023-05-17 08:29:19 -0700

    stuff

--- lua/config/global.lua
+++ lua/config/global.lua
@@ -83,7 +83,16 @@ vim.call('plug#end') ]]
 
 --Vimtex Stuff
 vim.g.tex_flavor = 'latex'
-vim.g.vimtex_view_method='skim'
+if(vim.loop.os_uname().sysname=='Macunix')
+then
+  vim.g.vimtex_view_method='skim'
+  --vim.g.vimtex_view_skim_activate=1
+  vim.g.vimtex_view_skim_sync=1
+  vim.g.vimtex_view_skim_reading_bar=1
+else
+  vim.g.vimtex_view_general_viewer='okular'
+  vim.g.vimtex_view_general_options = '--noraise --unique @pdf\\#src:@line@tex'
+end
 vim.g.vimtex_quickfix_mode = 0
 vim.opt_local.conceallevel = 2
 vim.g.tex_conceal='abdmg'
